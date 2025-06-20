9f43ddec2ff46cf4da5fe867ec24dab48ebb166c
Mike Lacy <mike.lacy@gmail.com>
2025-06-20 11:12:03 -0500

    Clean up file formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,26 +6,26 @@ import markdown from "@eslint/markdown"
 import stylistic from "@stylistic/eslint-plugin"
 
 export default defineConfig([
-  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
-  {
-    files: ["**/*.js", "**/*.mjs"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        Log: "readonly",
-        Module: "readonly",
-      },
+    { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+    {
+        files: ["**/*.js", "**/*.mjs"],
+        languageOptions: {
+            ecmaVersion: "latest",
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+                Log: "readonly",
+                Module: "readonly",
+            },
+        },
+        plugins: { js, stylistic },
+        extends: ["js/recommended", "stylistic/recommended"],
+        rules: {
+            "@stylistic/brace-style": ["error", "1tbs", { allowSingleLine: true }],
+            "@stylistic/comma-dangle": ["error", "only-multiline"],
+            "@stylistic/max-statements-per-line": ["error", { max: 2 }],
+            "@stylistic/quotes": ["error", "double"]
+        }
     },
-    plugins: { js, stylistic },
-    extends: ["js/recommended", "stylistic/recommended"],
-    rules: {
-      "@stylistic/brace-style": ["error", "1tbs", { allowSingleLine: true }],
-      "@stylistic/comma-dangle": ["error", "only-multiline"],
-      "@stylistic/max-statements-per-line": ["error", { max: 2 }],
-      "@stylistic/quotes": ["error", "double"]
-    }
-  },
-  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
+    { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
 ])
